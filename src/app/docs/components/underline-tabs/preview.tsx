75e8deff2038b75052ad0a77ecf82c5318d768f9
ThangDevAlone <quangthangvtlg@gmail.com>
2025-03-28 17:12:29 +0700

    🐛 :sparkles: Resolve build fail

--- src/app/docs/components/underline-tabs/preview.tsx
+++ src/app/docs/components/underline-tabs/preview.tsx
@@ -92,7 +92,7 @@ export const PreviewUnderlineTabs = () => {
         >
           <div className="absolute inset-0 bg-[radial-gradient(#80808080_1px,transparent_1px)] [background-size:16px_16px] opacity-50"></div>
           <div className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3 w-lg bg-background border p-8 rounded-md">
-            <UnderlineTabs key={key} tabs={tabs} defaultTab="profile" />
+            <UnderlineTabs key={key} tabs={tabs} defaultTabId="profile" />
           </div>
           <div className="absolute top-4 right-4 flex gap-2">
             <Button
